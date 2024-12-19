f935d430df0aa4a48de6a3033e167afbd2e85c82
CarsonHI <carson.healy@tis.edu.mo>
2024-12-19 11:25:36 +0800

    Put Logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,21 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGithub } from '@fortawesome/free-brands-svg-icons'; // GitHub and Envelope (Gmail) icons
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
-import { faCode } from '@fortawesome/free-solid-svg-icons'; // Code icon
+import { faGithub } from '@fortawesome/free-brands-svg-icons'; // GitHub icon
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'; // Envelope (Gmail) icon
+import Logo from '../assets/Logo.png'; 
 
 const Header = () => {
   return (
     <header className="bg-blue-300 text-white py-8 shadow-lg">
       <div className="max-w-7xl mx-auto flex flex-col justify-center items-center space-y-4">
-        {/* Code Icon and Name */}
+        {/* Logo */}
         <div className="flex items-center space-x-4">
-          <FontAwesomeIcon icon={faCode} className="text-white text-4xl" />
-          <h1 className="text-6xl font-extrabold text-gray-200 tracking-wider">Carson Healy</h1>
-          <FontAwesomeIcon icon={faCode} className="text-white text-4xl" />
+          {/* Logo without rounding */}
+          <img src={Logo} alt="Logo" className="w-16 h-16" /> {/* Removed rounded-full class */}
         </div>
         
         {/* Social Icons */}
-        <div className="flex space-x-6 text-5xl">
+        <div className="flex space-x-6 text-5xl mt-4">
           {/* GitHub Link */}
           <a href="https://github.com/CarsonHealy" target="_blank" rel="noopener noreferrer">
             <FontAwesomeIcon icon={faGithub} className="text-white hover:text-gray-300 transition-all duration-200" />
